b2b55eb7f5e2842e658d128d6d1d21a98b5840bd
khh <khh8500@naver.com>
2024-02-22 11:21:02 +0900

    삭제하기 ajax

---
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -0,0 +1,34 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+    private final BoardRepository boardRepository;
+
+    @DeleteMapping("/api/boards/{id}")
+    public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response) {
+        Board board = boardRepository.selectOne(id);
+        if (board == null) {
+            response.setStatus(404);
+            return new ApiUtil<>(404, "해당 게시글을 찾을 수 없습니다");
+        }
+        boardRepository.deleteById(id);
+        return new ApiUtil<>(null);
+    }
+
+    @GetMapping(value = "/api/boards")
+    public ApiUtil<?> findAll(HttpServletResponse response) {
+        //response.setStatus(200);
+        List<Board> boardList = boardRepository.selectAll();
+        return new ApiUtil<>(boardList); // MessageConverter
+    }
+}
